d49cbd30dee99805b5b1731b41baa4725f127f61
gaggudeep <gagandeepsinghjubbal@gmail.com>
2024-03-30 01:22:34 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
-const App = () => {
-  return <>3D developer portfolia</>;
-};
+import { BrowserRouter } from "react-router-dom"
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components"
 
-export default App;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="bg-primary relative z-0">
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
